60e84b80e5f3462810a406d7db7a5bf04d3d3f29
Sajjad Ahmad <jogimar14@gmail.com>
2020-10-27 17:22:39 +0500

    migrate User and add validations to it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates :username, :email, :password, presence: true
 end
---
+++ db/schema.rb
@@ -0,0 +1,11 @@
+ActiveRecord::Schema.define(version: 2020_10_27_121141) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
