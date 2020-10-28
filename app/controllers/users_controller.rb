67b727f297e9f9ed6d0ea0f1c610d180f41feadd
Sajjad Ahmad <jogimar14@gmail.com>
2020-10-28 19:33:00 +0500

    change index template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to @user
+      redirect_to :root
     else
       render :edit
     end
