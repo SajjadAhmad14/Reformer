f88025f2ed37c577a53c96f302c91a6f3775da3a
Sajjad Ahmad <jogimar14@gmail.com>
2020-10-28 14:44:13 +0500

    add a private method user_params in controller and changed new tempelate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    byebug
     @user = User.new(user_params)
     if @user.save
       redirect_to new_users_path
@@ -16,6 +15,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    p params.require(:user).permit(:username, :email, :password)
   end
+
 end
