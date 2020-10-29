109f2536a5c5de94241b5fb92ee34fcde8538fbd
Sajjad Ahmad <jogimar14@gmail.com>
2020-10-29 13:55:12 +0500

    change redirect path when user is created and change text to password tag_field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_users_path
+      redirect_to new_user_path
     else
       render :new
     end
